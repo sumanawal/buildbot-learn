641f1244201c6726d581ca802cfbda45948ee025
Suman Awal <suman@cloudfactory.com>
2016-11-18 14:53:03 +0545

    Refactor.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 
 using namespace std;
 
+int sum(int a, int b){
+  return a + b;
+}
+
+int subtract(int a, int b){
+  return a - b;
+}
 int main(){
-  int sum_result = sum(5, 6);
+  int sum_result = subtract(5, 6);
   cout << "Hello World !" << sum_result << endl;
   return 0;
 }
 
-int sum(int a, int b){
-  return a + b;
-}
+
